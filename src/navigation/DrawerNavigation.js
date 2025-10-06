250047c1bc219deae7316b7651415102ba2d0e9f
Nishant Jain <amoltdhage@gmail.com>
2025-10-06 23:59:48 +0530

    icon change in drawer

--- src/navigation/DrawerNavigation.js
+++ src/navigation/DrawerNavigation.js
@@ -1,54 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-// import BottomTabs from './BottomTabs';
-// import { useSelector } from 'react-redux';
-// import { fetchCollection } from '../Services/firestoreServices';
-// import UsersListScreen from '../screens/Authenticated/UsersListScreen';
-// import ApprovalCertificate from '../screens/Authenticated/ApprovalCertificate';
-
-// const Drawer = createDrawerNavigator();
-
-// export default function DrawerNav() {
-//     const userId = useSelector(state => state.auth?.user);
-//   const [userDetail, setUserDetail] = useState(null);
-
-//   useEffect(() => {
-//     if (userId && !userDetail) getUserDetail(userId);
-//   }, [userId]);
-
-//   const getUserDetail = async id => {
-//     const userData = await fetchCollection('users', id);
-//     setUserDetail(userData);
-//   };
-//   return (
-//     <Drawer.Navigator
-//       screenOptions={{
-//         headerShown: false, // Disable default header
-//         drawerStyle: {
-//           backgroundColor: '#002b5c',
-//           width: 250,
-//         },
-//         drawerLabelStyle: {
-//           color: 'white',
-//           fontSize: 16,
-//           fontWeight: '600',
-//         },
-//       }}
-//     >
-//       <Drawer.Screen
-//         name="MainTabs"
-//         component={BottomTabs}
-//         options={{ title: 'Home' }}
-//       />
-// {userDetail?.email?.toLowerCase() === 'nishantjain951@gmail.com' ||
-// userDetail?.email?.toLowerCase() === 'amoltd11@gmail.com' ? (
-//   <Drawer.Screen name="Attendees" component={UsersListScreen} />
-// ) : null}
-// <Drawer.Screen name="Approval Certificate" component={ApprovalCertificate} />
-//     </Drawer.Navigator>
-//   );
-// }
-
 import React, { useEffect, useState } from 'react';
 import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
 import {
@@ -67,6 +16,8 @@ import ApprovalCertificate from '../screens/Authenticated/ApprovalCertificate';
 import About from '../screens/Authenticated/About';
 import ContactUs from '../screens/Authenticated/ContactUs';
 import PaymentsScreen from '../screens/Authenticated/PaymentsScreen';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
 const Drawer = createDrawerNavigator();
 const { width } = Dimensions.get('window');
 
@@ -172,14 +123,14 @@ export default function DrawerNav() {
           <DrawerItem
             label={() => <DrawerLabel label="About Us" />}
             icon={({ size }) => (
-              <AntDesign name="poweroff" size={size} color="white" />
+              <AntDesign name="infocirlceo" size={size} color="white" />
             )}
             onPress={() => navigation.navigate('About')}
           />
           <DrawerItem
             label={() => <DrawerLabel label="ContactUs" />}
             icon={({ size }) => (
-              <AntDesign name="poweroff" size={size} color="white" />
+              <AntDesign name="team" size={size} color="white" />
             )}
             onPress={() => navigation.navigate('ContactUs')}
           />
@@ -187,7 +138,8 @@ export default function DrawerNav() {
           <DrawerItem
             label={() => <DrawerLabel label="Logout" />}
             icon={({ size }) => (
-              <AntDesign name="poweroff" size={size} color="white" />
+              <MaterialIcons name="logout" size={size} color="white" />
+              // <AntDesign name="poweroff" size={size} color="white" />
             )}
             onPress={() => handleLogout(navigation)}
           />
